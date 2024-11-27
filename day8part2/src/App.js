dde9fbff5c6120b0678d735133c8ea3a7fa173ef
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2024-11-27 23:12:30 +0530

    finalize app

--- day8part2/src/App.js
+++ day8part2/src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
-import {BrouseRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
-
+import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
+import Login from './components/Login';
+import Dasboard  from './components/Dashbord';
 
 function App() {
   return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/' element={<Dasboard/>}/>
+      </Routes>
+    </Router>
     
   );
 }
--- day8part2/src/components/Login.js
+++ day8part2/src/components/Login.js
@@ -1,8 +1,8 @@
 import {useState} from "react";
-import {UseNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Login(){
-    const navigate = UseNavigate();
+    const navigate = useNavigate();
     const [user,setUser] = useState({username: "", password: "", error: ""})
     const getTextinput =(e)=>{
         const {name,value}=e.target;
